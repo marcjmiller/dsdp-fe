6a486868e4ef32bd2dd4c42f0af2f8b92f41b121
abutt <anthony.butt@cse-corp.com>
2021-11-22 13:37:38 -0500

    Add file upload to backend

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import App from './App'
+import mockAxios from 'jest-mock-axios'
+
+afterEach(() => {
+	mockAxios.reset()
+})
 
 test('renders upload', () => {
-  render(<App />);
-  const uploadElement = screen.getByText(/Drag and drop a file here or click/i);
-  expect(uploadElement).toBeInTheDocument();
-});
+	render(<App />)
+	const uploadElement = screen.getByText(/Drag and drop a file here or click/i)
+	expect(uploadElement).toBeInTheDocument()
+})
+
+test('uploads a file', async () => {
+	render(<App />)
+	window.URL.createObjectURL = jest.fn().mockImplementation(() => 'url')
+	const uploadElement = screen.getByText(/Drag and drop a file here or click/i)
+	const file = new File(['file'], 'ping.json', {
+		type: 'application/json',
+	})
+	Object.defineProperty(uploadElement, 'files', {
+		value: [file],
+	})
+	fireEvent.drop(uploadElement)
+	fireEvent.change(uploadElement)
+	expect(await screen.findByText('ping.json')).toBeInTheDocument()
+	expect(mockAxios.post).toHaveBeenCalledWith(
+		'http://localhost:8080/api/files',
+		new FormData(),
+		{
+			headers: {
+				'Content-Type': 'multipart/form-data',
+			},
+		}
+	)
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { DropzoneArea } from 'material-ui-dropzone';
-import './App.css';
+import axios from 'axios'
+import { DropzoneArea } from 'material-ui-dropzone'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <DropzoneArea />
-    </div>
-  );
+	const handleFileUpload = async (files: File[]) => {
+		let formData = new FormData()
+		files.forEach((file) => formData.append('files', file))
+		axios.post('http://localhost:8080/api/files', formData, {
+			headers: { 'Content-Type': 'multipart/form-data' },
+		})
+	}
+	return (
+		<div className='App'>
+			<DropzoneArea showFileNames onChange={handleFileUpload} />
+		</div>
+	)
 }
 
 export default App
