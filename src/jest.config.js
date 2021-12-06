ce8c19be9774e16732db52ac1c878109d0478991
Ethan Pellegrini <ethan.p.pellegrini.civ@army.mil>
2021-12-06 10:37:41 -0500

    Add code coverage reporting
    Resolves #25

--- src/jest.config.js
+++
@@ -1,9 +0,0 @@
-const {defaults} = require('jest-config')
-// Sync object
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  testEnvironment: "jsdom",
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx']
-};
-
-module.exports = config;
