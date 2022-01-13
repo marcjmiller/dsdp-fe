6cb042a7481acba68bef435106a4c826c6cca0fe
harriscfn <harriscfn@gmail.com>
2022-01-13 10:42:07 -0500

    Adds dynamic baseURL for Axios controlled by REACT_APP_BASE_URL var

--- src/config/API.ts
+++ src/config/API.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:8080/api'
+const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:8080/api'
 
 const API = axios.create({
 	baseURL,
